cf78b58a1f79966fc6ae105dde15da7f48c251fa
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-01 18:51:06 -0700

    added comment for server.js and updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Note-Taker
+## Description 
+Note Taker is a web-based application that allows users to create, view, and manage notes. The application provides a simple and user-friendly interface for users to write, save, and delete notes. It is built using Node.js, Express.js, and HTML/CSS, making it easy to deploy and use.
+
+## Table of Contents
+            
+* [Installation](#installation)
+* [Usage](#usage)
+* [Deployed-Application-URL](#deployed-application-url)
+* [Mock-Up](#mock-up)
+* [Contributing](#contributing)
+* [Question](#question)
+            
+## Installation
+Run the following commend for installation.
+
+npm i
+            
+## Usage 
+Clone the repo and run the application with installation instruction.
+
+## Deployed-Application-URL
+[https://enigmatic-retreat-24327-db0de0ee7a05.herokuapp.com/]
+        
+## Mock-Up
+The following image show how the application should be look like when you are running.
+
+![Alt text](./public/assets/images/1.png)
+![Alt text](./public/assets/images/2.png)
+
+## Contributing
+Fork the repo and copy it into your GitHub.
+                 
+## Question
+If you have any questions about this repo, contact me directly at ahkarhein04@gmail.com. You can find more my work at (https://github.com/ahkar-hein)
--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+// import required package
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// Declare variable for port 3001 or process.env.PORT for heroku
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+// express middleway
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// get route for index.html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+// get route for notes.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
-
+// dummy array for user input (id, titles and text)
 let notes = [];
 
+// Read teh db.JSON file by using fs.readfile
 fs.readFile('db/db.json', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading db.json:', err);
@@ -30,21 +34,27 @@ fs.readFile('db/db.json', 'utf8', (err, data) => {
   }
 });
 
+// get route for /api/notes
 app.get('/api/notes', (req, res) => res.json(notes));
 
+// post route for /api/notes
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
   
+    // checking user input for title and text
     if (!title || !text) {
       return res.status(400).json({ error: 'Title and text fields are required.' });
     }
 
     const newNote = {
-      id: Date.now().toString(),
+      id: Date.now().toString(), // generate unique id for the note
       title,
       text,
     };
+    // putting id, title and text into dummy array
     notes.push(newNote);
+
+    // inserting user input data into db.JSON by using fs.writefile and JSON.stringify
     fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
         if (err) {
           console.error('Error writing db.json:', err);
@@ -54,9 +64,11 @@ app.post('/api/notes', (req, res) => {
         res.status(201).json(newNote);
       });
 });
+// note delete function
 app.delete('/api/notes/:id', (req, res) => {
+    // Get the ID of the note to be deleted from the request parameters
     const noteId = req.params.id;
-  
+  // Filter out the note with the given ID and update the 'notes' array
     notes = notes.filter((note) => note.id !== noteId);
   
     // Save the updated notes to db.json
@@ -69,3 +81,5 @@ app.delete('/api/notes/:id', (req, res) => {
       res.sendStatus(204);
     });
   });
+
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
